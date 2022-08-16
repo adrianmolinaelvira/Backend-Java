1a619d3be1cdc84289f5bf85c4e53fc17d388b05
Adrián Molina <adrian.molina@bosonit.com>
2022-08-16 11:45:47 +0200

    Inicio de Spring

---
+++ Ejercicio_Maven/src/main/java/app/Person.java
@@ -0,0 +1,43 @@
+package app;
+
+public class Person {
+
+    private String name, city;
+    private int age;
+
+    public Person(String name, String city, int age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+
+    @Override
+    public String toString(){
+        return "/// Nombre Completo: " + this.name + ", Ciudad: " + this.city + ", Edad: " + this.age+" ///";
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getCity(){
+        return this.city;
+    }
+
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setCity(String newCity){
+        this.city = newCity;
+    }
+
+}
